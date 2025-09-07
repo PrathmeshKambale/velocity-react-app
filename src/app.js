a8823878c4ce6070b6618065adb8144b1e873d7b
root <root@ip-172-31-46-88.ap-south-1.compute.internal>
2025-09-07 13:28:20 +0000

    velocity-react-app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from"react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Velocity 🚀</h1>
+      <p>This is a sample React project deployed via GitHub.</p>
+    </div>
+  );
+}
+
+export default App;
